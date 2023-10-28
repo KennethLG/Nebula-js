1f98c0235076bb1079ce911ff0b5690e4f8a1a6d
KennethLG <orsonleiziaga@gmail.com>
2023-10-28 11:23:58 -0500

    Refactor KeyboardManager and add SceneManager and EventManager to systems index

--- src/systems/KeyboardManager.ts
+++ src/systems/KeyboardManager.ts
@@ -1,22 +1,22 @@
 import EventManager from "./EventManager";
 
 export default class KeyboardManager {
-  private eventBus: EventManager;
-  private keys: { [key: string]: boolean } = {};
+  private eventManager: EventManager;
+  keys: { [key: string]: boolean } = {};
 
-  constructor(eventBus: EventManager) {
-    this.eventBus = eventBus;
+  constructor(eventManager: EventManager) {
+    this.eventManager = eventManager;
     window.addEventListener("keydown", this.onKeyDown.bind(this));
     window.addEventListener("keyup", this.onKeyUp.bind(this));
   }
 
-  private onKeyDown(event: KeyboardEvent) {
+  private onKeyDown(event: KeyboardEvent) {    
     this.keys[event.key] = true;
-    this.eventBus.dispatch("keydown", event.key);
+    this.eventManager.emit("keydown", event.key);
   }
 
   private onKeyUp(event: KeyboardEvent) {
     this.keys[event.key] = false;
-    this.eventBus.dispatch("keyup", event.key);
+    this.eventManager.emit("keyup", event.key);
   }
 }
---
+++ src/systems/SceneManager.ts
@@ -0,0 +1,21 @@
+import * as THREE from 'three';
+
+export default class SceneManager {
+  instances: any[];
+  scene: THREE.Scene;
+  constructor() {
+    this.scene = new THREE.Scene();
+    this.instances = [];
+  }
+
+  add(instance) {
+    this.scene.add(instance.mesh)
+    this.instances.push(instance);
+  }
+
+  update() {
+    this.instances.forEach(instance => {
+      instance.update();
+    })
+  }
+}
