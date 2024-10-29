0bef8824722545d2810804045643f5e069e1d2c3
KennethLG <orsonleiziaga@gmail.com>
2024-10-28 23:21:50 -0500

    Add event types and event map for socket communication

---
+++ src/systems/eventTypes/index.ts
@@ -0,0 +1,27 @@
+import { type IPlayer } from '@/components/Player';
+import {
+  type MatchFoundResponse,
+  type PlayerUpdatedResponse,
+} from '../http/responses/socketResponse';
+
+export const EventTypes = {
+  MatchFound: 'matchFound',
+  PlayerUpdated: 'playerUpdated',
+  MatchStart: 'matchStart',
+  GameOver: 'gameOver',
+  Keydown: 'keydown',
+  Keyup: 'keyup',
+  MovementKeydown: 'movementKeydown',
+} as const;
+
+export type EventType = (typeof EventTypes)[keyof typeof EventTypes];
+
+export interface EventMap {
+  [EventTypes.MatchFound]: MatchFoundResponse;
+  [EventTypes.PlayerUpdated]: PlayerUpdatedResponse;
+  [EventTypes.MatchStart]: { matchId: string; player: IPlayer };
+  [EventTypes.GameOver]: { playerId: number };
+  [EventTypes.Keydown]: string;
+  [EventTypes.Keyup]: string;
+  [EventTypes.MovementKeydown]: 'left' | 'right';
+}
